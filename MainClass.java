f789166a40e1ceca94d47b3b9c7a292031af5775
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:57:19 +0300

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package assignment1test;
-
-/**
- *
- * @author shaha
- */
 import java.util.*;
 
 class Node {
@@ -32,7 +21,7 @@ class Node {
     }
 }
 
-public class Assignment1test {
+public class MAinClass {
 
     public static Node deleteNodes(Node head, int m, int n) {
         Node ptr = head;
